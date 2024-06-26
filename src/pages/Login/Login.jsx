8bfd9cdd571d01d330dc4c5732f17e89ec11b060
Aolslekej <alexander_baranovsky@mail.ru>
2024-06-26 15:30:57 +0300

    pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import mail from "/mail-line.png";
+import eye from "/eye-line.png";
+import "./login.scss";
 
 export default function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ErrorPass, setErrorPass] = useState(false);
   return (
-    <div>Login</div>
-  )
+    <div className="Login">
+      <h1 className="welcome">Welcome back!</h1>
+      <form action="" className="log">
+        <label htmlFor="" className="label">
+          Your e-mail
+          <div className="input">
+            <input
+              type="text"
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="text"
+            />
+            <img src={mail} alt="" className="img" />
+          </div>
+        </label>
+        <label htmlFor="" className="label">
+          Password
+          <div className="input">
+            <input
+              type="password"
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="text"
+            />
+            <img src={eye} alt="" className="img" />
+          </div>
+          {ErrorPass && <h4 className="error">Не верный пароль</h4>}
+        </label>
+      </form>
+      <a href="" className="forgot">
+        Forgot Password?
+      </a>
+      <button className="log-button">Log in</button>
+      <div className="to-sign">
+        <p>Don’t have an account?</p>
+        <Link to="/registr" className="sign">
+          Sign up now.
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/Registr/Registr.jsx
+++ src/pages/Registr/Registr.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import mail from "/mail-line.png";
+import eye from "/eye-line.png";
+import "./registr.scss";
 
 export default function Registr() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  /*const auth = getAuth();
+
+  async function createUser(event) {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, Email, Password).then(
+      (currentUser) => {
+        console.log(currentUser);
+      }
+    );
+  }*/
+
   return (
-    <div>Registr</div>
-  )
+    <div className="Registr">
+      <h1 className="become">Become a member!</h1>
+      <form action="" className="reg">
+        <label htmlFor="" className="label">
+          Your e-mail
+          <div className="input">
+            <input
+              type="text"
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="text"
+            />
+            <img src={mail} alt="" className="img" />
+          </div>
+        </label>
+        <label htmlFor="" className="label">
+          Password
+          <div className="input">
+            <input
+              type="password"
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="text"
+            />
+            <img src={eye} alt="" className="img" />
+          </div>
+        </label>
+      </form>
+      <button className="reg-button">Sign up</button>
+      <div className="to-login">
+        <p>Already have an account?</p>
+        <Link to="/login" className="sign">
+          Log in.
+        </Link>
+      </div>
+    </div>
+  );
 }
